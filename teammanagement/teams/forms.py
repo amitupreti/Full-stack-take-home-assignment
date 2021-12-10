2a2f97b242c9e63300f698a8287903883fcb36bc
Amit Upreti <a.u.aua937@gmail.com>
2021-12-09 21:03:49 -0700

    working on forms and models

---
+++ teammanagement/teams/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+
+from teams.models import Team
+
+class TeamForm(forms.ModelForm):
+    class Meta:
+        model = Team
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'phone',
+            'role',
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            # print(field)
+            if field in ['role']:
+                self.fields[field].widget.attrs.update(
+                    {'data-toggle': 'toggle'})
+                self.fields[field].widget.attrs.update({'data-size': 'big'})
+                self.fields[field].widget.attrs.update(
+                    {'style': 'text-decoration:none!important;'})
+
+            else:
+                self.fields[field].widget.attrs.update(
+                    {'class': 'form-control'})
--- teammanagement/teams/views.py
+++ teammanagement/teams/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views.generic import (ListView, UpdateView, TemplateView, CreateView, DeleteView)
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+
+# class ListSportsBetAccounts(LoginRequiredMixin, ListView):
+#     """
+#     Lists all the bet account information
+#     """
+#
+#     model = SportsBetAccounts
+#     template_name = 'sportsbet/sportsbets_list.html'
