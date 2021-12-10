9afc77b922034ce95b7b236c3c029209ad830715
Amit Upreti <a.u.aua937@gmail.com>
2021-12-09 21:10:20 -0700

    working on teamplates

--- teammanagement/teams/views.py
+++ teammanagement/teams/views.py
@@ -4,12 +4,16 @@ from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import (ListView, UpdateView, TemplateView, CreateView, DeleteView)
 
+
+from .models import Team
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
-
+class ListTeam(ListView):
+    model = Team
+    template_name = 'teams/team_list.html'
 # class ListSportsBetAccounts(LoginRequiredMixin, ListView):
 #     """
 #     Lists all the bet account information
